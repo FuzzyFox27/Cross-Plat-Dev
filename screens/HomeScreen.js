0f5b41af3dee0ac6f32c410ba1d753264047b2e6
Alec Taylor <alectaylor1337@gmail.com>
2020-04-26 18:59:45 +0100

    before map components

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { Image, Platform, StyleSheet, Text, TouchableOpacity, View, Dimensions} from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import Svg, {image} from 'react-native-svg';
-import { Searchbar } from 'react-native-paper';
+import { Searchbar, TextInput } from 'react-native-paper';
 import MapView,{ Marker, Callout }  from 'react-native-maps';
 import { MonoText } from '../components/StyledText';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
@@ -12,14 +12,16 @@ export default function HomeScreen() {
   const [searchString, SetSearchString] = React.useState (null);
   return (
     <View style={styles.container}>
-      <GooglePlacesAutocomplete>
-      <View style={{backgroundColor: '#333', height: 150}}></View>
-      <Searchbar
-        placeholder="Search"
-        onChangeText={SetSearchString}
-        value={searchString}
-      />
-      
+      <GooglePlacesAutocomplete 
+      style = {styles.searcrhBar} 
+      currentLocation={true} 
+      currentLocationLabel="Current location" 
+      nearbyPlacesAPI='GooglePlacesSearch'
+      autoFocus={true}>
+        {({})>(
+          <React.Fragment>
+          </React.Fragment>
+        )}
       <MapView
       region={{
         latitude: 53.22683,
@@ -116,10 +118,6 @@ function DevelopmentModeNotice() {
   }
 }
 
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/workflow/development-mode/');
-}
-
 function handleHelpPress() {
   WebBrowser.openBrowserAsync(
     'https://docs.expo.io/versions/latest/get-started/create-a-new-app/#making-your-first-change'
@@ -137,86 +135,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+  searcrhBar:{
+    height: 90,
+    width: 300,
+    borderWidth: 1,
+    paddingHorizontal:16
+  }
 });
